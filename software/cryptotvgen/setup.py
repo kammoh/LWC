4cd0a55904d5fae7f27862851d5fb985581cb218
Kamyar Mohajerani <kammoh@gmail.com>
2020-08-10 22:21:58 -0400

    try support GA debian-latest Python 3.6?

--- software/cryptotvgen/setup.py
+++ software/cryptotvgen/setup.py
@@ -47,7 +47,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Production',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
@@ -59,7 +59,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.7+',
+        'Programming Language :: Python :: 3.6+',
     ],
 
     # What does your project relate to?
@@ -77,7 +77,10 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['cffi>=1.14.1'],
+    install_requires=[
+        "cffi>=1.14.1",
+        "importlib_resources;python_version<'3.7'"
+        ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
